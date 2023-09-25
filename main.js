7efcc9feafb7c99593a896e184e1d0db24ca000d
Petrik Diak <diak@petrik.hu>
2023-09-25 18:10:35 +0200

    JavaScript funkciók

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
 import './style.css'
+document.addEventListener('DOMContentLoaded',init);
+document.getElementById('btn_szamit').addEventListener('click',szamit);
+
+function init() {}
+
+function szamit() {
+  let a = parseFloat(document.getElementById('a-oldal').value);
+  let b = parseFloat(document.getElementById('b-oldal').value);
+  let c = parseFloat(document.getElementById('c-oldal').value);
+  if (a + b <= c || a + c <= b || b + c <= a){
+    document.getElementById('eredmeny').textContent = 'Hibás adatok! A háromszög nem szerkeszthető';
+  }
+  else {
+    const haromszog = {
+      a: a,
+      b: b,
+      c: c,
+      kerulet: function() {
+        return this.a+this.b+this.c;
+      },
+      terulet: function() {
+        const s  = this.kerulet() / 2;
+        return Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+      },
+      keruletEsTerulet() {
+        return `Terület: ${this.terulet()} - Kerület: ${this.kerulet()}`;
+      }
+    };
+    console.log(haromszog);
+    document.getElementById('eredmeny').textContent = haromszog.keruletEsTerulet();
+  }
+}
